557f3450d355fd4b932d6647c039753f6d68e096
Riatre <fz258921@gmail.com>
2025-07-05 04:49:24 +0800

    nvim: +mason -tagbar

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -73,7 +73,6 @@ unpack(disable_in_vscode {
   'simnalamburt/vim-mundo',
   't9md/vim-choosewin',
   'qpkorr/vim-bufkill',
-  'majutsushi/tagbar',
   'will133/vim-dirdiff',
   'tpope/vim-eunuch',
   {'nvim-telescope/telescope.nvim', tag = '0.1.8',
@@ -83,4 +82,5 @@ unpack(disable_in_vscode {
   }},
   {'nvim-telescope/telescope-fzf-native.nvim', 
     build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+  {'mason-org/mason.nvim', opts = {}},
 })}
